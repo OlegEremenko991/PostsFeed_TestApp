92971225698cd4f700c8ed76e292595c0dc375fd
Oleg991 <o.n.eremenko@gmail.com>
2020-10-10 17:22:42 +0300

    added weak self to image load

--- PostsFeed_TestApp/Network/UIImageView_Extension.swift
+++ PostsFeed_TestApp/Network/UIImageView_Extension.swift
@@ -9,18 +9,18 @@ import UIKit
 
 extension UIImageView {
     
-    // Load image from URL on background thread
+    // Load image from URL on background thread and update UI on main thread
     
     func load(url: URL) {
         let queue = DispatchQueue.global(qos: .utility)
         queue.async {
             if let data = try? Data(contentsOf: url) {
-                DispatchQueue.main.async {
-                    self.image = UIImage(data: data)
+                DispatchQueue.main.async { [weak self] in
+                    self?.image = UIImage(data: data)
                 }
             } else {
-                DispatchQueue.main.async {
-                    self.image = UIImage(systemName: "person")
+                DispatchQueue.main.async { [weak self] in
+                    self?.image = UIImage(systemName: "person")
                 }
             }
         }
