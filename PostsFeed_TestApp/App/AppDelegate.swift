a3cea91df4ba82a48253be92075cd17d823a5dc9
Oleg991 <o.n.eremenko@gmail.com>
2021-02-24 11:43:24 +0400

    App launch moved from AppDelegate to SceneDelegate

--- PostsFeed_TestApp/App/AppDelegate.swift
+++ PostsFeed_TestApp/App/AppDelegate.swift
@@ -8,18 +8,19 @@
 import UIKit
 
 @main
-class AppDelegate: UIResponder, UIApplicationDelegate {
-    
-    var window: UIWindow?
+final class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
-
-        window = UIWindow(frame: UIScreen.main.bounds)
-        let rootViewController = MainVC()
-        let navigationController = UINavigationController(rootViewController: rootViewController)
-        window?.rootViewController = navigationController
-        window?.makeKeyAndVisible()
+        // Override point for customization after application launch.
         return true
+    }
+
+    // MARK: UISceneSession Lifecycle
+
+    func application(_ application: UIApplication, configurationForConnecting connectingSceneSession: UISceneSession, options: UIScene.ConnectionOptions) -> UISceneConfiguration {
+        // Called when a new scene session is being created.
+        // Use this method to select a configuration to create the new scene with.
+        return UISceneConfiguration(name: "Default Configuration", sessionRole: connectingSceneSession.role)
     }
 
 }
